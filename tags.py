fbe402c788805aeb8dfa0c796490aa53b83ef203
Dhanavarshini Sankar Ganesh <49709308+Dhanavarsh1@users.noreply.github.com>
2024-07-12 20:58:52 -0700

    tags.py

--- tags.py
+++ tags.py
@@ -1,7 +1,6 @@
 from TikTokApi import TikTokApi
 import json
 import asyncio
-import pandas as pd
 
 def get_cookies_from_file():
     with open('cookies.json') as f:
@@ -19,19 +18,16 @@ def get_cookies(**kwargs):
     return cookies
 
 async def main(tag_name, count):
-    nicknames = []
     async with TikTokApi() as api:
         api._get_cookies = get_cookies
         await api.create_sessions()
         tag = api.hashtag(name=tag_name)
         async for vid in tag.videos(count=count):
-            vid_dict = vid.as_dict
-            nickname = vid_dict['author']['nickname']
-            nicknames.append(nickname)
-    
-    # Create a DataFrame from the nicknames list
-    df = pd.DataFrame(nicknames, columns=['Nickname'])
-    print(df)
+            print(vid)
+            print(vid.as_dict)
+            print(vid.nickname)
+            print('--------------')
+        print()
 
 if __name__ == "__main__":
     asyncio.run(main(tag_name="funny", count=20))
